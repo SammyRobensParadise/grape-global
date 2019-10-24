76156d3a6ed4560d4064a893503de697be56440d
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-10-23 21:10:29 -0700

    added text fields

--- grape-global/src/lib/index.js
+++ grape-global/src/lib/index.js
@@ -2,4 +2,5 @@ import TextInput from "./TextInput";
 import Button from "./global-buttons/buttons/Button";
 import MultiButton from "./global-buttons/buttons/MultiButton";
 import Chevron from './global-buttons/chevrons/Chevron'
-export { TextInput, Button, MultiButton, Chevron };
+import SingleInput from './global-text-inputs/text-inputs/SingleInput'
+export { TextInput, Button, MultiButton, Chevron, SingleInput };
